c595d07ecb6538ea4e8a841a4cdd77cb8591f170
NikitinStanislav <nikitinsa13@gmail.com>
2024-08-21 21:23:49 -0400

    22 -Chain Rule, single and multiple values

--- src/main/java/calculus/Calculus.java
+++ src/main/java/calculus/Calculus.java
@@ -15,6 +15,13 @@ public class Calculus {
         return x * x;
     }
 
+    public static double func3(double y1, double y2) {
+        return y1 * y2 + 4.7 * y1;
+    }
+
+    public static double func4(double x) {
+        return func1(x) * func2(x) + 4.7 * func1(x);
+    }
 
     /**we want to take change in output and divide it
      by change in X, and it gives us the measure of gradient of the function at that particular point*/
@@ -27,12 +34,46 @@ public class Calculus {
     }
 
     public static void main(String[] args) {
-        for (double x = -2; x < 2; x += 0.1){
+
+        double x = 2.78;
+        double y1 = func1(x);
+        double y2 = func2(x);
+        double z = func3(y1, y2);
+
+        double dy1dx = differentiate(Calculus::func1, x);   //rate of change
+        double dy2dx = differentiate(Calculus::func2, x);   //rate of change
+        double dzdy1 = differentiate(y -> func3(y, y2), y1);
+        double dzdy2 = differentiate(y -> func3(y1, y), y2);
+
+        double dzdxCalculated = (dzdy1 * dy1dx) + (dzdy2 * dy2dx);
+        double dzdxApproximated = differentiate(Calculus::func4, x);
+
+        System.out.println(dzdxCalculated);
+        System.out.println(dzdxApproximated);
+
+
+
+/***
+ *      Single variable chain rule example
+        double dydx = differentiate(Calculus::func1, x);   //rate of change
+        double dzdy = differentiate(Calculus::func2, y);
+        double dzdx = differentiate(Calculus::func3, x);
+
+        System.out.println(dydx);
+        System.out.println(dzdy);
+        System.out.println(dzdx);
+
+        System.out.println(dzdy * dydx);*/
+
+
+
+
+        /*for (double x = -2; x < 2; x += 0.1){
 
             //double gradient = differentiate(Calculus::func1, x);   //for linear function func1 you will see sema gradient
             double gradient = differentiate(Calculus::func2, x); //just a random rule: gradient of x^2 is 2x
 
             System.out.printf("%.2f\t%.2f\n", x, gradient);
-        }
+        }*/
     }
 }
